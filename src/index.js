449284075c345461e9f08390fb7af35aa2462cd2
Katia Nienow <nienowkatia@gmail.com>
2023-06-21 00:15:38 -0300

    req 02

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 const PORT = process.env.PORT || '3001';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -20,4 +21,16 @@ app.get('/talker', async (req, res) => {
   const data = await fs.readFile('/app/src/talker.json', 'utf-8');
   const talkers = JSON.parse(data);
   return res.status(HTTP_OK_STATUS).json(talkers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = JSON.parse( await fs.readFile('/app/src/talker.json', 'utf-8'));
+  const findTalkerById = data.find((talker) => Number(talker.id) === Number(id)); 
+  if(!findTalkerById) {
+    res.status(HTTP_NOT_FOUND_STATUS).json({
+      message: 'Pessoa palestrante não encontrada'
+    });
+  }
+  return res.status(HTTP_OK_STATUS).json(findTalkerById);
 });
