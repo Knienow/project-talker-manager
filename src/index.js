25b795f069d9c6f5571979a656bd43d4eb77a70a
Katia Nienow <nienowkatia@gmail.com>
2023-06-27 01:09:04 -0300

    requisito 06 em andamento

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs/promises');
 const crypto = require('crypto');
 const path = require('path');
-const { body, validationResult, header } = require('express-validator');
+const { body, validationResult, header, param } = require('express-validator');
 
 const app = express();
 app.use(express.json());
@@ -74,7 +74,7 @@ app.post('/login', [
 });
 
 // REQUISITO 05
-const teste = [
+const validators = [
   header('authorization').notEmpty().withMessage('Token não encontrado'),
   header('authorization').isLength({ min: 16, max: 16 })
   .withMessage('Token inválido'),
@@ -92,7 +92,7 @@ const teste = [
   validateDateDDMMYYYY('talk.watchedAt'),
 ];
 
-app.post('/talker', teste, async (req, res) => {
+app.post('/talker', validators, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const message = errors.errors[0].msg;
@@ -113,33 +113,51 @@ app.post('/talker', teste, async (req, res) => {
 });
 
 // REQUISITO 06 - só rascunho 
-app.put('/talker/:id', [  
-  header('authorization').notEmpty().withMessage('Token não encontrado'),
-  header('authorization').isLength({ min: 16, max: 16 })
-  .withMessage('Token inválido'),
-  body('name').notEmpty().withMessage('O campo "name" é obrigatório'),
-  body('name').isLength({ min: 3 })
-  .withMessage('O "name" deve ter pelo menos 3 caracteres'), 
-  body('age').notEmpty().withMessage('O campo "age" é obrigatório'),
-  body('age').isInt({ min: 18 })
-  .withMessage('O campo "age" deve ser um número inteiro igual ou maior que 18'),
-  body('talk').notEmpty().withMessage('O campo "talk" é obrigatório'),
-  body('talk.watchedAt').notEmpty().withMessage('O campo "watchedAt" é obrigatório'),
-  body('talk.watchedAt').isDate()
-  .withMessage('O campo "watchedAt" deve ter o formato "dd/mm/aaaa"'),
-  body('talk.rate').notEmpty().withMessage('O campo "rate" é obrigatório'),
-  body('talk.rate').isInt({ min: 1, max: 5 })
-  .withMessage('O campo "rate" deve ser um número inteiro entre 1 e 5'),
-  body('id').notEmpty().withMessage('Pessoa palestrante não encontrada'),
+ // param('id').notEmpty().withMessage('Pessoa palestrante não encontrada')
+//  
+app.put('/talker/:id', validators, [
+  param('id').notEmpty().withMessage('Pessoa palestrante não encontrada'),
 ], async (req, res) => {
+  const errors = validationResult(req);
+  const { id } = req.params;
+  const { name, age, talk: watchedAt, rate } = req.body;
+  if (!errors.isEmpty()) {
+    const message = errors.errors[0].msg;
+    const { authorization } = req.headers;
+    if (!authorization) {
+      return res.status(HTTP_UNAUTHORIZED).json({ message });
+    } 
+    if (authorization.length !== 16) {
+      return res.status(HTTP_UNAUTHORIZED).json({ message });
+    } 
+    if (!id) {
+      return res.status(HTTP_NOT_FOUND_STATUS).json({ message });
+    }
+    return res.status(HTTP_BAD_REQUEST).json({ message });
+  }
   const talkers = await talkersList();
-  return res.status(HTTP_OK_STATUS).json(talkers);
+  const index = talkers.findIndex((element) => element.id === Number(id));
+  talkers[index] = { id: Number(id), name, age, talk: watchedAt, rate };
+  await fs.writeFile(route, JSON.stringify(talkers));
+  return res.status(HTTP_OK_STATUS).json(talkers[index]);
 });
 
 // REQUISITO 07 - só rascunho 
 app.delete('/talker/:id', [
   header('authorization').notEmpty().withMessage('Token não encontrado'),
   header('authorization').isLength({ min: 16, max: 16 }).withMessage('Token inválido'),
-], async (req, res) => 
-// const talkers = await talkersList();
-res.status(HTTP_NO_CONTENT).json());
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const message = errors.errors[0].msg;
+    const { authorization } = req.headers;
+    if (!authorization) {
+      return res.status(HTTP_UNAUTHORIZED).json({ message });
+    } 
+    if (authorization.length !== 16) {
+      return res.status(HTTP_UNAUTHORIZED).json({ message });
+    }
+  const talkers = await talkersList();
+  res.status(HTTP_NO_CONTENT).json(talkers);
+  } 
+});
